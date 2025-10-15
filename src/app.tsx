16892a3435aa7e0d7f3005f2ee6dc115b49f3abb
octavio__augusto/ <135380814+octaviosilva4275@users.noreply.github.com>
2025-10-15 13:52:00 -0300

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,1 +1,10 @@
+import UpsellPage from "./components/UpsellPage";
 
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/upsell" element={<UpsellPage />} />
+    </Routes>
+  );
+}
